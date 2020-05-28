caa7b55ecabd3692d6a539c3d4ed87f8e156fa32
Sujay <32679803+sujay48@users.noreply.github.com>
2020-05-28 17:18:32 +0530

    23_May_2020

---
+++ coding_solutions/plus_one.py
@@ -0,0 +1,25 @@
+"""
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+"""
+
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        carry = 1
+        for i in range(len(digits) - 1, -1, -1):
+            carry += digits[i]
+            digits[i] = carry % 10
+            carry //= 10
+
+        if carry:
+            digits.insert(0, carry)
+
+        return digits
