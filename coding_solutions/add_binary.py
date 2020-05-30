415d114bbe23bc1533872c473b8d7034855db0aa
Sujay <32679803+sujay48@users.noreply.github.com>
2020-05-30 13:48:15 +0530

    29_May_2020

---
+++ coding_solutions/add_binary.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        carry = 0
+        result = ''
+        a = list(a)
+        b = list(b)
+
+        while a or b:
+
+            if a:
+                carry += int(a.pop())
+
+            if b:
+                carry += int(b.pop())
+
+            result += str(carry % 2)
+            carry //= 2
+
+        if carry:
+            result += str(carry % 2)
+
+        return result[::-1]
